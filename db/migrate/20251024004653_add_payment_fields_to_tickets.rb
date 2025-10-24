4a1a9dd5023992c1a4bc363f1eafd8318a812599
NestumMilo-isFezan <nurahfezanbinnordin@gmail.com>
2025-10-24 08:59:18 +0800

    fix: remove previous redundant migration files

--- db/migrate/20251024004653_add_payment_fields_to_tickets.rb
+++
@@ -1,7 +0,0 @@
-class AddPaymentFieldsToTickets < ActiveRecord::Migration[8.0]
-  def change
-    add_column :tickets, :payment_screenshot_url, :string
-    add_column :tickets, :transaction_id, :string
-    add_column :tickets, :payment_method, :string
-  end
-end
