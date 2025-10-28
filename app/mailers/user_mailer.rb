d9ee08b4eeb9fd32ad4e7a723b5543d12088ba56
NestumMilo-isFezan <nurahfezanbinnordin@gmail.com>
2025-10-28 16:10:28 +0800

    fix: avoid spam keyword in the email content

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,14 @@ class UserMailer < ApplicationMailer
     @code = code
     @expires_in = 15 # minutes
 
+    headers['Reply-To'] = 'support@saleschatalyst.com'
+    headers['List-Unsubscribe'] = "<mailto:unsubscribe@saleschatalyst.com>, <https://eventzflow.com/unsubscribe?u=#{@user.id}>"
+    headers['List-Unsubscribe-Post'] = 'List-Unsubscribe=One-Click'
+
     mail(
+      from: 'EventzFlow Support <support@saleschatalyst.com>',
       to: @user.email,
-      subject: 'Verify Your Email Address'
+      subject: 'Verify your email address'
     )
   end
 end
