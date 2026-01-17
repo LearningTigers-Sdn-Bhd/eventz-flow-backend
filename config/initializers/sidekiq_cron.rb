c7aeb3e64d6fd73180f34082771f546eba7f8cde
Nurahfezan Nordin <nurahfezanbinnordin@gmail.com>
2026-01-17 14:33:15 +0800

    chore: add last 30 days session cleanup scheduler

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -5,5 +5,11 @@ Rails.application.config.after_initialize do
       cron: '0 * * * *', # or cron: '5 * * * *'  to runs at :05 past each hour
       class: 'CompleteEndedEventsJob'
     )
+
+    Sidekiq::Cron::Job.create(
+      name: 'Cleanup expired sessions - daily at 3am',
+      cron: '0 3 * * *',
+      class: 'CleanupExpiredSessionsJob'
+    )
   end
 end
