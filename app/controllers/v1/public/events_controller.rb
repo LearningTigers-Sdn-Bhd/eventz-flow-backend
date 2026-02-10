0e272810a9a9a2e3a812a9896ae0294abc8b992d
Orson Tamin <orson.t99@gmail.com>
2026-02-10 15:22:17 +0800

    fix: fix business matching events error in public link

--- app/controllers/v1/public/events_controller.rb
+++ app/controllers/v1/public/events_controller.rb
@@ -34,9 +34,9 @@ module V1
         error_response(message: 'Event not found', status: :not_found)
       end
 
-      # GET /v1/public/events/:id/business_matching_events
+      # GET /v1/public/events/:slug/business_matching_events
       def business_matching_events
-        @event = Event.find_by!(id: params[:id])
+        @event = Event.friendly.find(params[:slug])
         
         unless @event.use_business_matching
           return render json: { errors: "Business matching is not enabled for this event" }, status: :bad_request
